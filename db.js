c6abcbe483f03c3a506ab393ffa1d1715050ba72
Bishnu Mishra <mishravishnu369@gmail.com>
2024-09-25 03:13:29 +0530

    setup mongodb connection

--- db.js
+++ db.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
+var mongoURL = "mongodb+srv://bishnumishra1109:ShreeRam@123@cluster0.vtyquxg.mongodb.net/food-delivery-app";
 
+mongoose.connect(mongoURL, {useUnifiedTopoly: true, useNewUrlParser: true})
+
+var db = mongoose.connection
+
+db.on('connection', () => {
+    console.log(`MongoDB connection successfull`);
+})
+
+db.on('error', () => {
+    console.log(`MongoDB connection failed`);
+})
+
+module.exports = mongoose
