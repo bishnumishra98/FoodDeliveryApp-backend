9798b483bf4994537d98f846d61518ae7a90b6cd
Bishnu Mishra <mishravishnu369@gmail.com>
2024-10-16 04:23:38 +0530

    installed cors

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const Pizza  = require('./models/pizzaModel');
 const db = require("./db");
+const cors = require('cors');
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',   // allow requests from the React app
+    credentials: true   // if you want to allow cookies or credentials
+}));
 
 const pizzasRoute = require('./routes/pizzaRoute');
 app.use('./api/pizzas/', pizzasRoute);
