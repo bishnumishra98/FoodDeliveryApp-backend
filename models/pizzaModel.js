8d4ae90cc60f3280a46649a2cc3b196386c4faf7
Bishnu Mishra <mishravishnu369@gmail.com>
2024-11-25 08:04:57 +0530

    changed schema for PizzaModel due to change in variant to size

--- models/pizzaModel.js
+++ models/pizzaModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const pizzaSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    variants: [{ type: String }],
-    prices: [],
+    size: { type: String, required: true },
+    price: { type: Number, required: true },
     category: { type: String, required: true },
     image: { type: String, required: true },
     description: { type: String, required: true }
@@ -11,6 +11,6 @@ const pizzaSchema = new mongoose.Schema({
     timestamps: true,  // Adds createdAt and updatedAt fields automatically
 });
 
-const pizzaModel = mongoose.model('pizza', pizzaSchema);
+const pizzaModel = mongoose.model('pizza', pizzaSchema);   // "Pizza" is the collection name
 
 module.exports = pizzaModel;
