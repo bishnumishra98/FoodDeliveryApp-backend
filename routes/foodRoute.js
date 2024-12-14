6e7cb790ef251aa616358cec46cff7b9fa7ae3da
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-14 07:13:22 +0530

    added /addfood route in foodRoute.js

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -11,4 +11,23 @@ router.get("/getallfoods", async (req, res) => {
 	}
 });
 
+router.post("/addfood", async (req, res) => {
+    const food = req.body.food;
+
+	try {
+		const newfood = new Food({
+			name: food.name,
+			size: food.size,
+			price: food.price,
+			category: food.category,
+			image: food.image,
+			description: food.description,
+		})
+		await newfood.save()
+		res.send('New food added successfully')
+	} catch (error) {
+		return res.status(400).json({ message: error });
+	}
+});
+
 module.exports = router;
