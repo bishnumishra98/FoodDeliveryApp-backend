e8f6d0ebc9745433d3a09c7d22adac6857d8c7e9
Bishnu Mishra <mishravishnu369@gmail.com>
2024-12-16 04:17:23 +0530

    added a new filed isSuperAdmin in userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema({
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         isAdmin: { type: Boolean, required: true, default: false },
+        isSuperAdmin: { type: Boolean, required: true, default: false },
     }, {
         timestamps: true,
     }
